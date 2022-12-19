4d457932960f2fd46a94322f952805d1e1ed536e
juliomchn <litonfire1@gmail.com>
2022-12-20 00:02:48 +0100

    Creada la estructura de la base de datos

---
+++ src/main/java/com/pruebalavanderia/jmachr/app/springbootpruebalavanderia/models/enitity/Carro.java
@@ -0,0 +1,76 @@
+package com.pruebalavanderia.jmachr.app.springbootpruebalavanderia.models.enitity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity()
+@Table(name="carros")
+public class Carro implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Cliente cliente;
+
+    private Double peso;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_Creacion")
+    private Date createAt;
+
+    @PrePersist
+    public void prePersist(){
+        createAt = new Date();
+    }
+    //private User usuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    private static final long serialVersionUID = 1L;
+
+
+}
--- src/main/java/com/pruebalavanderia/jmachr/app/springbootpruebalavanderia/models/enitity/Cliente.java
+++ src/main/java/com/pruebalavanderia/jmachr/app/springbootpruebalavanderia/models/enitity/Cliente.java
@@ -2,11 +2,14 @@ package com.pruebalavanderia.jmachr.app.springbootpruebalavanderia.models.enitit
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -15,7 +18,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "clientes")
@@ -41,7 +46,15 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 
-	public Long getId() {
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Carro> carros;
+
+    
+	public Cliente() {
+        carros = new ArrayList<Carro>();
+    }
+
+    public Long getId() {
 		return id;
 	}
 
@@ -86,6 +99,18 @@ public class Cliente implements Serializable {
 	}
 
 	private static final long serialVersionUID = 1L;
+
+    public List<Carro> getCarros() {
+        return carros;
+    }
+
+    public void setCarros(List<Carro> carros) {
+        this.carros = carros;
+    }
+
+    public void addCarros(Carro carro){
+        carros.add(carro);
+    }
 
 }
 
